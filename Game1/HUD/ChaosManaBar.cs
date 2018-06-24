dc056a9c4204da64fd5648e97ef0c59846f8fbc3
Omnigazer <omnigazer@gmail.com>
2018-06-25 01:12:09 +0300

    Hide mana bars if there's no mana of that type

--- Game1/HUD/ChaosManaBar.cs
+++ Game1/HUD/ChaosManaBar.cs
@@ -16,29 +16,32 @@ namespace Omniplatformer.HUD
 
         public override void Draw()
         {
-            var spriteBatch = GraphicsService.Instance;
-            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
-            // int bar_border_thickness = 5;
-            Point border_size = new Point(Thickness, Thickness);
-            Point size = new Point(Width, Height);
-            Point current_size = new Point((int)((Width - Thickness * 2) * (Player.CurrentMana[ManaType.Chaos] / Player.MaxMana[ManaType.Chaos])),
-                Height - Thickness * 2);
+            if (Player.MaxMana(ManaType.Chaos) > 0)
+            {
+                var spriteBatch = GraphicsService.Instance;
+                spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+                // int bar_border_thickness = 5;
+                Point border_size = new Point(Thickness, Thickness);
+                Point size = new Point(Width, Height);
+                Point current_size = new Point((int)((Width - Thickness * 2) * (Player.CurrentMana[ManaType.Chaos] / Player.MaxMana(ManaType.Chaos))),
+                    Height - Thickness * 2);
 
-            Rectangle outer_rect = new Rectangle(Position, size);
-            Rectangle inner_rect = new Rectangle(Position + border_size, current_size);
+                Rectangle outer_rect = new Rectangle(Position, size);
+                Rectangle inner_rect = new Rectangle(Position + border_size, current_size);
 
-            var source_rect = new Rectangle((int)(bar_loop / loop_period), 0, GameContent.Instance.testLiquid.Width / 2, GameContent.Instance.testLiquid.Height);
-            spriteBatch.Draw(GameContent.Instance.whitePixel, outer_rect, Color.Gray);
+                var source_rect = new Rectangle((int)(bar_loop / loop_period), 0, GameContent.Instance.testLiquid.Width / 2, GameContent.Instance.testLiquid.Height);
+                spriteBatch.Draw(GameContent.Instance.whitePixel, outer_rect, Color.Gray);
 
-            // Apply the distort effect
-            ApplyDistort();
+                // Apply the distort effect
+                ApplyDistort();
 
-            spriteBatch.Draw(GameContent.Instance.testLiquid, inner_rect, source_rect, Color.FromNonPremultiplied(255, 80, 20, 255));
-            // draw caustics over the bar
-            source_rect = new Rectangle(0, 0, GameContent.Instance.causticsMap.Width, GameContent.Instance.causticsMap.Height / 4);
-            //spriteBatch.Draw(GameContent.Instance.causticsMap, inner_rect, source_rect, Color.Orange);
-            spriteBatch.End();
-            base.Draw();
+                spriteBatch.Draw(GameContent.Instance.testLiquid, inner_rect, source_rect, Color.FromNonPremultiplied(255, 80, 20, 255));
+                // draw caustics over the bar
+                source_rect = new Rectangle(0, 0, GameContent.Instance.causticsMap.Width, GameContent.Instance.causticsMap.Height / 4);
+                //spriteBatch.Draw(GameContent.Instance.causticsMap, inner_rect, source_rect, Color.Orange);
+                spriteBatch.End();
+                base.Draw();
+            }
         }
     }
 }
