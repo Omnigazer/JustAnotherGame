cb8e865b9b626317d37106b26257f50198239930
Omnigazer <omnigazer@gmail.com>
2018-07-16 19:21:01 +0300

    First draft of texture rendering of editor's current block

--- Game1/GraphicsService.cs
+++ Game1/GraphicsService.cs
@@ -34,6 +34,30 @@ namespace Omniplatformer
             // DrawGame(texture, rect, color, rotation, new Vector2(1, 1));
         }
 
+        public static void DrawScreen(Texture2D texture, Rectangle rect, Color color, float rotation, Vector2 clamped_origin, float scale = 1, bool tiled = false)
+        {
+            rect = new Rectangle(rect.X, rect.Y, (int)(rect.Width * scale), (int)(rect.Height * scale));
+            // clamped_origin = new Vector2(clamped_origin.X, 1 - clamped_origin.Y);
+            var bounds = texture.Bounds;
+            if (tiled)
+            {
+                bounds.Size = new Point((int)(rect.Size.X / scale), (int)(rect.Size.Y / scale));
+                // bounds.Inflate(0 * bounds.Width, 0 * bounds.Height);
+            }
+
+
+            // var origin = new Vector2(texture.Bounds.Width * clamped_origin.X, texture.Bounds.Height * clamped_origin.Y);
+            var origin = new Vector2(bounds.Width * clamped_origin.X, bounds.Height * clamped_origin.Y);
+            var screen_rect = rect;// game.GameToScreen(rect, clamped_origin);
+            //if (origin.Length() > 0)
+               // screen_rect.Offset(rect.Size.X * clamped_origin.X, rect.Size.Y * clamped_origin.Y);
+            // if (clamped_origin.Length() > 0) { if (origin.Length() > 0) { } }
+            // var bounds = texture.Bounds;
+            // bounds.Inflate((x_tiles - 1) * bounds.Width,  (y_tiles - 1) * bounds.Height);
+            Instance.Draw(texture: texture, destinationRectangle: screen_rect, color: color, rotation: rotation, origin: origin,
+                effects: SpriteEffects.None, layerDepth: 0, sourceRectangle: bounds); // default parameters
+        }
+
         public static void DrawGame(Texture2D texture, Rectangle rect, Color color, float rotation, Vector2 clamped_origin, bool tiled = false)
         {
             clamped_origin = new Vector2(clamped_origin.X, 1 - clamped_origin.Y);
