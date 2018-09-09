f0c6e00eab7f9f8576bd679b2c8ae809603dc7e7
Omnigazer <omnigazer@gmail.com>
2018-09-09 19:08:50 +0300

    Added rotation to Firebolt projectile

--- Game1/Spells/FireBolt.cs
+++ Game1/Spells/FireBolt.cs
@@ -11,22 +11,6 @@ namespace Omniplatformer.Spells
 {
     class FireBolt
     {
-        /*
-        public static void Cast(Player player)
-        {
-            if (player.SpendMana(ManaType.Chaos, 1))
-            {
-                PositionComponent pos = (PositionComponent)player;
-                var projectile = new FireBoltProjectile(pos.WorldPosition.Center, new Vector2(5, 5), player);
-                var movable = (CharMoveComponent)player;
-                var proj_movable = projectile.GetComponent<ProjectileMoveComponent>();
-                int dir_sign = (int)pos.WorldPosition.face_direction;
-                proj_movable.direction = new Vector2(15 * dir_sign, 0);
-                GameService.Instance.RegisterObject(projectile);
-            }
-        }
-        */
-
         public static void Cast(Character caster, Position target)
         {
             if (caster.SpendMana(ManaType.Chaos, 1))
@@ -37,10 +21,11 @@ namespace Omniplatformer.Spells
                 var proj_movable = projectile.GetComponent<ProjectileMoveComponent>();
                 // int dir_sign = (int)pos.WorldPosition.face_direction;
                 // Vector2 direction = new Vector2()
-                float speed = 15;
+                float speed = 20;
                 Vector2 direction = target.Coords - pos.WorldPosition.Coords;
                 direction.Normalize();
                 // proj_movable.direction = new Vector2(15 * dir_sign, 0);
+                proj_movable.Rotate(-(float)Math.Atan2(direction.Y, direction.X));
                 proj_movable.Direction = speed * direction;
                 GameService.Instance.AddToMainScene(projectile);
             }
