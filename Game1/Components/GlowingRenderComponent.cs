f92a5a7c3b16b8bbb8eee84e709227fb3b62a133
Omnigazer <omnigazer@gmail.com>
2018-09-13 12:36:32 +0300

    Added radius to GlowingRenderComponent

--- Game1/Components/GlowingRenderComponent.cs
+++ Game1/Components/GlowingRenderComponent.cs
@@ -11,12 +11,18 @@ namespace Omniplatformer.Components
     public class GlowingRenderComponent : RenderComponent
     {
         public Color GlowColor { get; set; } = Color.Orange;
+        public int Radius { get; set; } = 100;
         public GlowingRenderComponent(GameObject obj) : base(obj)
         {
 
         }
 
         public GlowingRenderComponent(GameObject obj, Color color) : base(obj, color)
+        {
+
+        }
+
+        public GlowingRenderComponent(GameObject obj, Color color, Texture2D texture) : base(obj, color, texture)
         {
 
         }
@@ -31,7 +37,7 @@ namespace Omniplatformer.Components
             //var projectile = projectiles[i];
             var pos = GetComponent<PositionComponent>();
             // var mask_halfsize = new Vector2(100, 100);
-            var mask_halfsize = pos.WorldPosition.halfsize + new Vector2(100, 100);
+            var mask_halfsize = pos.WorldPosition.halfsize + new Vector2(Radius);
             var rect = new Rectangle((pos.WorldPosition.Center - mask_halfsize).ToPoint(), (mask_halfsize * 2).ToPoint());
             // spriteBatch.Draw(lightMask, GameToScreen(rect), GetLightColor());
             var lightMask = GameContent.Instance.lightMask;
