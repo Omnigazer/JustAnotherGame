9783712378a2e50a90a514e0fab5c42633e51ce2
Omnigazer <omnigazer@gmail.com>
2018-09-13 12:37:31 +0300

    Added sparks to FireBoltProjectile

--- Game1/Objects/FireBoltProjectile.cs
+++ Game1/Objects/FireBoltProjectile.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Omniplatformer.Components;
+using Omniplatformer.Objects;
 using Omniplatformer.Utility;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +14,49 @@ namespace Omniplatformer
 {
     public class FireBoltProjectile : Projectile
     {
+        IEnumerator behavior;
         public FireBoltProjectile(Vector2 center, Vector2 halfsize, GameObject source = null): base(center, halfsize, source)
         {
             Team = source.Team;
-            Components.Add(new GlowingRenderComponent(this));
             Components.Add(new ProjectileMoveComponent(this, center, halfsize));
+            Components.Add(new GlowingRenderComponent(this));
             Components.Add(new DamageHitComponent(this, damage: 1));
+            behavior = behaviorGen();
         }
 
         public override object AsJson()
         {
             return new { type = GetType().AssemblyQualifiedName, Position = PositionJson.ToJson(this) };
+        }
+
+        public override void Tick(float time_scale)
+        {
+            current_time_scale = time_scale;
+            behavior.MoveNext();
+            base.Tick(time_scale);
+        }
+
+        public void GenerateSpark()
+        {
+            var movable = GetComponent<ProjectileMoveComponent>();
+            int x = RandomGen.Next(-15, 15), y = RandomGen.Next(-15, 15);
+            var spark = new Particle(movable.WorldPosition.Coords, new Vector2(1), new Vector2(x, y));
+            Game.AddToMainScene(spark);
+        }
+
+        float current_time_scale;
+        IEnumerator behaviorGen()
+        {
+            var movable = GetComponent<DynamicPhysicsComponent>();
+            while (true)
+            {
+                float wait_time = RandomGen.NextFloat(2, 8);
+                for (float t = 0; t < wait_time; t += current_time_scale)
+                {
+                    yield return null;
+                }
+                GenerateSpark();
+            }
         }
     }
 }
---
+++ Game1/Objects/Particle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Omniplatformer.Components;
+
+namespace Omniplatformer.Objects
+{
+    class Particle : Projectile
+    {
+        public Particle(Vector2 coords, Vector2 halfsize, Vector2 direction): base(coords, halfsize)
+        {
+            TTL = 50;
+            var movable = new DynamicPhysicsComponent(this, coords, halfsize) { Solid = false, Hittable = false, InverseMass = 10 };
+            movable.CurrentMovement = direction;
+            Components.Add(movable);
+            Components.Add(new GlowingRenderComponent(this) { Radius = 20 });
+        }
+    }
+}
