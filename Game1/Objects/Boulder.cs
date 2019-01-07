b05d43837b24f90222fd8ed1817241fe5abf94a9
Omnigazer <omnigazer@gmail.com>
2019-01-07 17:20:32 +0300

    Added fadeout animation for Boulder

--- Game1/Objects/Boulder.cs
+++ Game1/Objects/Boulder.cs
@@ -16,8 +16,23 @@ namespace Omniplatformer.Objects
             var movable = new ProjectileMoveComponent(this, coords, new Vector2(10, 10)) { Solid = false, Hittable = false, InverseMass = 0.5f };
             movable.CurrentMovement = direction;
             Components.Add(movable);
-            Components.Add(new RenderComponent(this) { Texture = GameContent.Instance.boulder });
+            var c = new AnimatedRenderComponent(this) { Texture = GameContent.Instance.boulder };
+            c.AddAnimation(new Animations.DeathAnimation(c));
+            Components.Add(c);
             Components.Add(new DamageHitComponent(this, damage: 3));
+        }
+
+        public override void onDestroy()
+        {
+            var drawable = GetComponent<AnimatedRenderComponent>();
+            if (drawable != null)
+            {
+                drawable._onAnimationEnd += (sender, e) => {
+                    if (e.animation == AnimationType.Death)
+                        base.onDestroy();
+                };
+                drawable.StartAnimation(AnimationType.Death, 50);
+            }
         }
     }
 }
